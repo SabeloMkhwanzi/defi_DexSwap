53d1d3de4ab89b413bfd888b5b9bf993c5e4462d
Sabelo Mhkwanazi <67804592+SabeloMkhwanzi@users.noreply.github.com>
2021-10-19 16:24:41 +0200

    update infonavbar

--- components/InfoButton.tsx
+++ components/InfoButton.tsx
@@ -29,11 +29,11 @@ const InfoButton = () => {
         {/* Information Section */}
         <Menu>
           <MenuButton
-            bg='gray.900'
+            bg='gray.600'
             as={Button}
             maxW="12"
             paddingRight="6"
-            rightIcon={<AiOutlineEllipsis size="40px" />}>
+            rightIcon={<AiOutlineEllipsis color="white" size="40px" />}>
                 </MenuButton>
                 <MenuList>
                     <MenuItem>About</MenuItem>
